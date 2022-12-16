adc5a6910dd327574cd3c4a2f69aacbc0f9f3ab9
Winstein Martins <sriwinstein@gmail.com>
2022-12-16 11:11:52 -0300

    Class task solution

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import ExpenseItem from "./components/ExpenseItem";
+import ExpensesList from "./components/ExpensesList";
 
 const App = () => {
   const expenses = [
@@ -24,28 +24,7 @@ const App = () => {
   ];
   
   return (
-    <div>
-      <ExpenseItem
-        title={expenses[0].title}
-        date={expenses[0].date}
-        amount={expenses[0].amount}
-      />
-      <ExpenseItem
-        title={expenses[1].title}
-        date={expenses[1].date}
-        amount={expenses[1].amount}
-      />
-      <ExpenseItem
-        title={expenses[2].title}
-        date={expenses[2].date}
-        amount={expenses[2].amount}
-      />
-      <ExpenseItem
-        title={expenses[3].title}
-        date={expenses[3].date}
-        amount={expenses[3].amount}
-      />
-    </div>
+    <ExpensesList expenses={expenses} />
   );
 };
 
